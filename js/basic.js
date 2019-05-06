b95a27560578b2fb40541c665f5da52403eb65e3
Musa <tamzid101@gmail.com>
2019-05-06 20:31:04 +0600

    map is not using layer object.

--- js/basic.js
+++ js/basic.js
@@ -1,29 +1,28 @@
 
 
 function initializeMap() {
+
+    let layer = new Layer(
+        `http://{s}.tile.osm.org/{z}/{x}/{y}.png`
+    );
     var m = new Map("mapid" , {
             center: [23.777, 90.399452],
             zoom: 30
-    });
+    } , layer);
 
 }
 
 
 class Map {
 
-    constructor(id , options , layer = undefined) {
-        console.log(options);
+    constructor(id , options , layer) {
 
         this.map = L.map(mapid , options);
-
-        if(layer === undefined)
-            this.setLayer(`http://{s}.tile.osm.org/{z}/{x}/{y}.png`);
-        else
-            this.setLayer(layer);
-
+        this.setLayer(layer);
+        console.log(layer);
     }
 
-    setLayer(layer) {
-        L.tileLayer(layer).addTo(this.map);
+    setLayer(layer ) {
+        L.tileLayer(layer.link , layer.options).addTo(this.map);
     }
 }
