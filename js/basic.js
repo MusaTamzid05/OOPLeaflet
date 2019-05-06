53f0ab607df99f6c723c216066ef7f6f5219d606
Musa <tamzid101@gmail.com>
2019-05-07 00:52:00 +0600

    basic.js renamed to map.js

--- js/basic.js
+++ js/basic.js
@@ -1,6 +1,5 @@
 
 
-
 function initializeMap() {
 
     let layer = new Layer(
@@ -82,41 +81,4 @@ function initializeMap() {
     );
 
 
-}
-
-
-class Map {
-
-    constructor(id , options , layer , markersAttr = []) {
-
-        this.map = L.map(mapid , options);
-        this.setLayer(layer);
-        this.markers = [];
-
-
-        for(let markerAttr of markersAttr) {
-            this.setMarker(markerAttr);
-        }
-
-    }
-
-    setLayer(layer ) {
-        L.tileLayer(layer.link , layer.options).addTo(this.map);
-    }
-
-    setMarker(markerAttr) {
-
-        console.log(markerAttr.popupParameters);
-            let marker = new Marker(
-                markerAttr.coordinates ,
-                this ,
-                markerAttr.shape ,
-                markerAttr.popupParameters ,
-                markerAttr.options);
-    }
-
-
-    addEvent(eventName , callback) {
-        this.map.on(eventName , callback);
-    }
 }
---
+++ js/map.js
@@ -0,0 +1,41 @@
+
+
+class Map {
+
+    constructor(id , options , layer , markersAttr = []) {
+
+        this.map = L.map(mapid , options);
+        this.setLayer(layer);
+        this.markers = [];
+
+
+        for(let markerAttr of markersAttr) {
+            this.setMarker(markerAttr);
+        }
+
+    }
+
+    setLayer(layer ) {
+        L.tileLayer(layer.link , layer.options).addTo(this.map);
+    }
+
+    setMarker(markerAttr) {
+
+        console.log(markerAttr.popupParameters);
+            let marker = new Marker(
+                markerAttr.coordinates ,
+                this ,
+                markerAttr.shape ,
+                markerAttr.popupParameters ,
+                markerAttr.options);
+    }
+
+
+    addEvent(eventName , callback) {
+        this.map.on(eventName , callback);
+    }
+
+    addGeoJSON(geoJSONFeature) {
+        L.geoJSON(geoJSONFeature).addTo(this.map.map);
+    }
+}
